94d242d4037d334c49354cfe9fa18514d3309012
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-19 20:21:40 +0000

    Register keypress_read as callable from another package

--- src/init.c
+++ src/init.c
@@ -5,4 +5,5 @@
 
 void R_init_keypress(DllInfo *info) {
   R_RegisterCCallable("keypress", "keypress", (DL_FUNC) keypress);
+  R_RegisterCCallable("keypress_read", "keypress_read", (DL_FUNC) keypress_read);
 }
